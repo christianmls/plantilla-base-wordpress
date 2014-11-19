249636c1a3122b10300575bf3b87ea5a40cc77fc
Cmaginet <info@cmaginet.com>
2014-11-19 11:38:31 -0500

    Inicio del Proyecto

---
+++ head/head.php
@@ -0,0 +1,15 @@
+<meta charset="<?php bloginfo( 'charset' ); ?>" />
+<title><?php wp_title(); ?></title>
+
+<link rel="icon" href="<?php bloginfo('template_url')?>/img/favicon.ico" type="image/x-icon">
+<link rel="shortcut icon" href="<?php bloginfo('template_url')?>/img/favicon.ico" type="image/x-icon" />
+
+<meta name="author" content="cmaginet.com">
+<meta name="description" content="<?php bloginfo( 'description' ); ?>">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<?php
+	remove_action('wp_head', 'wp_generator');
+	remove_action('wp_head', 'wlwmanifest_link');
+	remove_action('wp_head', 'rsd_link');	
+	wp_head();
+?>	
---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+	<!-- Obtener HEAD -->
+	<?php include(TEMPLATEPATH.'/head/'.'head.php');?>
+	<!-- Librerias CSS -->
+	<?php include(TEMPLATEPATH.'/head/'.'css_librerias.php');?>
+</head>
+<body>	
+	<?php include(TEMPLATEPATH.'/header/'.'header.php');?>
+	<?php include(TEMPLATEPATH.'/section/'.'section.php');?>
+	<?php include(TEMPLATEPATH.'/footer/'.'foot.php');?>
+</body>
+</html>

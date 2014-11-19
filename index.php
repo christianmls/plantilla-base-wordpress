c4a1a108ac797f3ecd239513fbc1f4303864bf21
Cmaginet <info@cmaginet.com>
2014-11-19 11:51:51 -0500

    VBootstrap

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 	<?php include(TEMPLATEPATH.'/head/'.'head.php');?>
 	<?php include(TEMPLATEPATH.'/head/'.'css_librerias.php');?>
 </head>
-<body>	
+<body class="container-fluid">
 	<?php include(TEMPLATEPATH.'/header/'.'header.php');?>
 	<?php include(TEMPLATEPATH.'/section/'.'section.php');?>
 	<?php include(TEMPLATEPATH.'/footer/'.'foot.php');?>
